3009024eba78466eeed7ab27995c93deb17e6717
Amit Kumar Mahto <amit.mahto3097@gmail.com>
2023-10-09 13:03:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  //   console.log("scroll");
+
+  header.classList.toggle('sticky', window.scrollY > 120);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('active');
+};
+
+menu.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navlist.classList.remove('active');
+};
